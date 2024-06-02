6746bd227fe37e6b50958b6bd168f7570ae54257
azpiero <joek0624@gmail.com>
2024-06-02 09:32:30 +0900

    update: :art: show updating status

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
-import { PlusCircle } from 'lucide-react';
+import { Loader2, PlusCircle } from 'lucide-react';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { Input } from '@/components/ui/input';
@@ -68,7 +68,12 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
   };
 
   return (
-    <div className='mt-6 border bg-slate-100 rounded-md p-4'>
+    <div className='relative mt-6 border bg-slate-100 rounded-md p-4'>
+      {isUpdating && (
+        <div className='absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center'>
+          <Loader2 className='animate-spin h-6 w-6 text-sky-700' />
+        </div>
+      )}
       <div className='font-medium flex items-center justify-between'>
         Course chapters
         <Button variant='ghost' onClick={toggleCreating}>
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd';
 
 import { cn } from '@/lib/utils';
-import { Grid, Grip, Pencil } from 'lucide-react';
+import { Grip, Pencil } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 
 interface ChaptersListProps {
